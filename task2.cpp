30b40b331f7ac494af06d5e8670f4db2212e7059
drqmahmoud <32275858+drqmahmoud@users.noreply.github.com>
2023-07-09 09:31:01 -0400

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,19 @@
+// Copyright 2023
+// Add a description....
+// ...
+// Author: your name
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+// add comments
+
+int main() {
+
+    cout << "Welcome to the wonders and of heads and tails" << endl;
+
+    return (0);
+}
